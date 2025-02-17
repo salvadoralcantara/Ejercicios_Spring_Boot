207a86bf14db7d1aa26f2d665d9934042885989b
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-17 15:35:23 -0600

    Agregando prayecto base sistema de gestion de alumnos hecho en Spring Initializr

---
+++ SistemaDeGestionAlumnos/src/main/java/SistemaDeGestionAlumnos/SistemaDeGestionAlumnosApplication.java
@@ -0,0 +1,13 @@
+package SistemaDeGestionAlumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionAlumnosApplication.class, args);
+	}
+
+}
---
+++ SistemaDeGestionAlumnos/src/test/java/SistemaDeGestionAlumnos/SistemaDeGestionAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package SistemaDeGestionAlumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
