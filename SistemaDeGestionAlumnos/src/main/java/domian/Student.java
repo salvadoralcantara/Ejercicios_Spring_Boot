d030b2f91f5af03e68a4ceeb782378c18376a415
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-17 15:46:23 -0600

    Agregando clase Student como domino

---
+++ SistemaDeGestionAlumnos/src/main/java/domian/Student.java
@@ -0,0 +1,13 @@
+package domian;
+
+public class Student {
+    private int id;
+    private String name;
+    private String gmail;
+    private int age;
+    private String course;
+
+    public Student() {
+    }
+
+}
