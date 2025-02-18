53acd51af046174ce76700a5a42b03c969a4a47d
Salvador Alcántara <salvadoralcantara626@gmail.com>
2025-02-17 21:28:23 -0600

    Clase Student completa y lista de estudiantes en StudentController

--- SistemaDeGestionAlumnos/src/main/java/controllers/StudentController.java
+++ SistemaDeGestionAlumnos/src/main/java/controllers/StudentController.java
@@ -1,4 +1,28 @@
 package controllers;
 
+import domian.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Arrays;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
-}
+        private List<Student> students = new ArrayList<>(Arrays.asList(
+                new Student(1, "Salvador", "salvador@gmail.com", 21, " course of Java"),
+                new Student(1, "Juan", "juan@gmail.com", 19, " course of python"),
+                new Student(1, "Maria", "maria@gmail.com", 42, " course of JavaScript"),
+                new Student(1, "Alec", "Alec@gmail.com", 23, " course of SQL")
+        ));
+
+
+
+    }
+
+
+
--- SistemaDeGestionAlumnos/src/main/java/domian/Student.java
+++ SistemaDeGestionAlumnos/src/main/java/domian/Student.java
@@ -10,4 +10,43 @@ public class Student {
     public Student() {
     }
 
+    public Student(int id, String name, String gmail, int age, String course){
+        this.id = id;
+        this.name = name;
+        this.gmail = gmail;
+        this.age = age;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
